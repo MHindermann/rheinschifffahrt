3d491c672540e186c0e760016e875cae7b6b1cdd
MHindermann <maximilian.hindermann@unibas.ch>
2022-02-16 22:24:36 +0100

    simple ner

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("bla")
+import spacy
+from collections import Counter
+
+nlp = spacy.load("de_core_news_lg") # python -m spacy download de_core_news_lg
+
+with open("full_transcription.txt", "r") as file:
+    raw_text = file.read()#.replace('\n', '') use both options
+
+doc = nlp(raw_text)
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+print(len(doc.ents))
+
+for attribute in ["PER", "LOC", "ORG", "MISC"]:
+    unique = [(x.text, x.label_) for x in doc.ents if x.label_ == attribute]
+    norm = set((x.text, x.label_) for x in doc.ents if x.label_ == attribute)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print(f"{attribute} unique {len(unique)}")
+    print(f"{attribute} norm {len(norm)}")
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    for x in norm:
+        print(x)
+
+#todo: map texts to document; map characters to document so that entities can be attributed to document
