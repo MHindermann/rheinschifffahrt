605bc22b081e2dc56860e0fbe09a13a15f1fc64f
antheajeanne <94174603+antheajeanne@users.noreply.github.com>
2022-02-28 17:08:59 +0100

    Update spacy_update.R
    added comments that describe what a set of commands does

--- files/spacy_update.R
+++ files/spacy_update.R
@@ -7,20 +7,25 @@ library(readtext)
 mypath <- "/Users/antheaalberto/switchdrive/RISE/Veranstaltungen/20220301_BGSH/Rheinschifffahrt Showcase/transcriptions/txt"
 setwd(mypath)
 
+## create a list of all .txt-files in directory and read them into the environment
 txt_files_ls = list.files(path=mypath, pattern="[0-9].txt") 
 
 rhein_txt <- readtext(txt_files_ls, 
                        docvarsfrom = "filenames")
+
+## create "link": name of image/scanned page
 rhein_txt$image <- rhein_txt$doc_id
 rhein_txt$image <- gsub(".txt", ".jpg", rhein_txt$image)
 rownames(rhein_txt) <- rhein_txt$image
 
+## open spacy and parse text
 spacy_initialize(model = "de_core_news_sm")
 parsed_txt <- spacy_parse(rhein_txt$text)
 head(parsed_txt)
 
+# extract named entities
 rhein_entity <- entity_extract(parsed_txt)
-persons <- rhein_entity[rhein_entity$entity_type=="PER",]
+#persons <- rhein_entity[rhein_entity$entity_type=="PER",]
 
 ## creating a link (e.g. 0002.jpg) to merge it to images and metadata
 rhein_entity$link <- gsub("text","", rhein_entity$doc_id)
